91e3a47dbb377d303673a6ea0986bc31362223e1
Vishnuvardhan Nayakam <vishnuvardhan.nayakam@gspann.com>
2019-07-09 11:11:50 +0530

    css properties are added

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ var CandidateSchema = new Schema({
     last_name : String,
     email : String,
     mobile_no : Number,
-    country_intrested : String
+    country_intrested : String,
+    bank_intrested : String
 
 })
 // making index to avoid duplicates 
@@ -59,106 +60,110 @@ var transporter = nodemailer.createTransport({
 app.use("/static", express.static("static"));
 app.set("view engine", "ejs");
 
-app.get("/", function(req,res){
-    res.render("index")
-});
-app.get("/emi", function(req, res){
-    res.render("index");
-})
+let loan_data;
+Details.find({}, function(err, data){
+    loan_data = data;
+    if(err) throw err;
+    for(i=0;i<data.length;i++)
+    console.log(data[i].bank_name,data[i])
 
-app.post("/emi", urlencodedParser, function(req, res){
-    p = req.body.loan_amount;
-    t = req.body.period;
-    r = req.body.intrest_rate;
-    var emi=0;
-    emi = fun(p,r,t);
+    //routes
 
-    var total_amount = emi *t;
-    var intrest = total_amount-p;
-    // Number(Math.round(emi+'e2')+'e-2'  -> this line to get the 2 decimals 
-    var min =0
-    if(r>0 && (Number(r)-5)>0)
-    min = Number(r)-5;
-    var max = Number(r)+5;
-    let loan_data;
-    Details.find({}, function(err, data){
-        loan_data = data;
-        if(err) throw err;
-        res.render("result",{emi : Number(Math.round(emi+'e2')+'e-2'),
-        intrest : Number(Math.round(intrest+'e2')+'e-2'),
-        total_amount : Number(Math.round(total_amount+'e2')+'e-2'),
-        intrest_rate : r,
-        principle_amount:p,
-        period : t,
-        min : min,
-        max : max,
-        data: loan_data
-        })
+    app.get("/", function(req,res){
+        res.render("index")
     });
-});
-app.get("/student_tips", function(req, res){
-    res.render("student_tips")
-});
-app.get("/compare_indian_loans", function(req,res){
-    let loan_data;
-    Details.find({}, function(err, data){
-        loan_data = data;
-        if(err) throw err;
-        for(i=0;i<data.length;i++)
-        console.log(data[i].bank_name,data[i])
+    app.get("/emi", function(req, res){
+        res.render("index");
+    })
 
+    app.post("/emi", urlencodedParser, function(req, res){
+        p = req.body.loan_amount;
+        t = req.body.period;
+        r = req.body.intrest_rate;
+        var emi=0;
+        emi = fun(p,r,t);
+
+        var total_amount = emi *t;
+        var intrest = total_amount-p;
+        // Number(Math.round(emi+'e2')+'e-2'  -> this line to get the 2 decimals 
+        var min =0
+        if(r>0 && (Number(r)-5)>0)
+        min = Number(r)-5;
+        var max = Number(r)+5;
+        let loan_data;
+        Details.find({}, function(err, data){
+            loan_data = data;
+            if(err) throw err;
+            res.render("result",{emi : Number(Math.round(emi+'e2')+'e-2'),
+            intrest : Number(Math.round(intrest+'e2')+'e-2'),
+            total_amount : Number(Math.round(total_amount+'e2')+'e-2'),
+            intrest_rate : r,
+            principle_amount:p,
+            period : t,
+            min : min,
+            max : max,
+            data: loan_data
+            })
+        });
+    });
+    app.get("/student_tips", function(req, res){
+        res.render("student_tips")
+    });
+    app.get("/compare_indian_loans", function(req,res){
         res.render("compare_bank_rates_i", {data : loan_data})
-    }).sort({bank_intrest_rate:1 })
-});
-
-app.get("/emi/apply-loan", function(req, res){
-    res.render("apply_bank_loan", {bol:false});
-});
-
-app.post("/emi/apply-loan",urlencodedParser, function(req, res){
-    var email_id = req.body.email;
-    var fname = req.body.fname;
-    var lname = req.body.lname;
-    var candidate = fname+" "+lname;
-    var mobile_number = req.body.pno;
-    var country = req.body.countries;
-    var candidate_pushtodb = new Candidates({
-        first_name : fname,
-        last_name : lname,
-        email : email_id,
-        mobile_no : mobile_number,
-        country_intrested : country
     });
-    
-    candidate_pushtodb.save(function (err, result_candi) {
-        if (err) return console.error(err);
-            console.log(result_candi.first_name+" "+result_candi.last_name + " is pushed to contact_details collection.");
+
+    app.get("/emi/apply-loan", function(req, res){
+        res.render("apply_bank_loan", {bol:false, data: loan_data}); 
     });
-    var mailOptions = {
-        from: 'foreignadmits1@gmail.com',
-        to: email_id,
-        subject: 'Thanks for contacting foreignadmits',
-        html: '<h4>Dear '+ candidate +'</h4><p>Thanks for consulting \
-        Foreign admits.</p>\n\n<p>Foreign admits '+ country +' counslting officer will\
-        contact you shortly.</p>\n\n<p>Regards,<br><b>Foreign Admits</b><br>\
-        foreignadmits1@gmail.com</p>'
-      }
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
+
+    app.post("/emi/apply-loan",urlencodedParser, function(req, res){
+        var email_id = req.body.email;
+        var fname = req.body.fname;
+        var lname = req.body.lname;
+        var candidate = fname+" "+lname;
+        var mobile_number = req.body.pno;
+        var country = req.body.countries;
+        var bank = req.body.banks;
+        var candidate_pushtodb = new Candidates({
+            first_name : fname,
+            last_name : lname,
+            email : email_id,
+            mobile_no : mobile_number,
+            country_intrested : country,
+            bank_intrested : bank   
+        });
+        
+        candidate_pushtodb.save(function (err, result_candi) {
+            if (err) return console.error(err);
+                console.log(result_candi.first_name+" "+result_candi.last_name + " is pushed to contact_details collection.");
+        });
+        var mailOptions = {
+            from: 'foreignadmits1@gmail.com',
+            to: email_id,
+            subject: 'Thanks for contacting foreignadmits',
+            html: '<h4>Dear '+ candidate +'</h4><p>Thanks for consulting \
+            Foreign admits.</p>\n\n<p>Foreign admits '+ country +' counslting officer will\
+            contact you shortly.</p>\n\n<p>Regards,<br><b>Foreign Admits</b><br>\
+            foreignadmits1@gmail.com</p>'
         }
-      });
-    res.render("apply_bank_loan", {bol:true});
-});
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+            console.log(error);
+            } else {
+            console.log('Email sent: ' + info.response);
+            }
+        });
+        res.render("apply_bank_loan", {bol:true, data:loan_data});
+    });
 
-
+}).sort({bank_intrest_rate:1 });
 
 
 
 
 var port  = process.env.PORT || 8080;
 console.log(port);
+
 
 var server  = app.listen(port);
